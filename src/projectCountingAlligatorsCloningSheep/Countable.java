08ab6577a1f284e18cff3fcc01bc54947323c6e6
vanessamnoble <vanessamnoble@icloud.com>
2017-01-23 11:29:56 -0600

    Countable Constructor

---
+++ src/projectCountingAlligatorsCloningSheep/Countable.java
@@ -0,0 +1,17 @@
+package projectCountingAlligatorsCloningSheep;
+
+/**
+ * Created by vanessamnoble on 1/20/17.
+ */
+
+public interface  Countable {
+
+    void incrementCount();
+
+    void resetCount();
+
+    public int getCount();
+
+    String getCountString();
+
+}
